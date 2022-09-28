eda87f062da0585cba7c4cc8273c0dd6fb5e6101
Saramantha <106452539+Saramantha@users.noreply.github.com>
2022-09-28 20:19:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ var item=document.getElementById("addItem").value;
     menuListArray.sort();
     htmldata=""
     for(var i=0;i<menuListArray.length;i++) {
-        htmldaa=htmldata+imgtags+ menuListArray[i]+'<br>';
+        htmldata=htmldata+imgtags+ menuListArray[i]+'<br>';
     }
-    document.getElementById("displayAddedMenu").serHTML = htmldata;
+    document.getElementById("displayAddedMenu").innerHTML = htmldata;
 
     }
 
